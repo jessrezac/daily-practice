4b2ef157577f0c0f56874ebc2ece30cbd9027d90
Jess Rezac <jess.rezac@gmail.com>
2019-08-28 07:04:15 -0500

    write get and post routes for new journals

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -2,7 +2,32 @@ class JournalController < ApplicationController
 
     get '/journals' do
         @journals = current_user.journals
-        erb :journals
+        erb :'journals/index'
+    end
+
+    get '/journals/new' do
+        erb :'journal/new'
+    end
+
+    post '/journals' do
+        journal = Journal.create(
+            title: params[:journal][:title], 
+            date: params[:journal][:date], 
+            content: params[:journal][:content],
+            user_id: current_user.id )
+        redirect to "/journals/#{journal.id}"
+    end
+
+    get '/journals/:id' do
+        @journal = Journal.find(params[:id])
+        "This is #{@journal.title}"
+    end
+
+    helpers do
+        def break_lines(text)
+            text.to_s.gsub(/\\n/, '<br/>')
+        end
+
     end
 
 end
