73b574ef3964e7eab83f9e27c7a3412bcf76e193
Jess Rezac <jess.rezac@gmail.com>
2019-08-28 22:40:58 -0500

    allow Journal updates and deletes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
-25.times do
+Journal.destroy_all
+User.destroy_all
+
+5.times do
     user = User.create(
         name: Faker::Name.name,
         email: Faker::Internet.safe_email,
         username: Faker::Internet.username,
         password: "testtest")
     
-    15.times do
+    10.times do
         create_date = Faker::Date.backward(days: 300)
         Journal.create(
             title: Faker::TvShows::Buffy.episode,
             date: create_date,
-            content: Faker::Lorem.paragraphs(number: 3).join("/n/n"),
+            content: Faker::Hipster.paragraphs(number: 3).join("\r\n"),
             user_id: user.id,
             created_at: create_date,
             updated_at: create_date)
