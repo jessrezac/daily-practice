39dbd86668b60f70e8d95d81746376fd90fdd2fa
Jess Rezac <jess.rezac@gmail.com>
2019-09-14 07:40:40 -0500

    set order for signup validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-    has_secure_password
     has_many :journals
 
-    validates :username, uniqueness: true
     validates :email, uniqueness: true
+    validates :username, uniqueness: true
+    has_secure_password
+
 end
